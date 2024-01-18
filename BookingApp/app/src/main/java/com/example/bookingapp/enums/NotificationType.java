def0eb5450886ec51d7bab4e7dffd94a3500d34c
milamilovic <milamilovic03@gmail.com>
2024-01-18 18:55:50 +0100

    notification screen

---
+++ BookingApp/app/src/main/java/com/example/bookingapp/enums/NotificationType.java
@@ -0,0 +1,9 @@
+package com.example.bookingapp.enums;
+
+public enum NotificationType {
+    RESERVATION_REQUEST,
+    RESERVATION_CANCELLATION,
+    OWNER_RATING,
+    ACCOMMODATION_RATING,
+    RESERVATION_APPROVE
+}
